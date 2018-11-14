08c65d263de1076383e37faa8f9abeb598ec3f22
Liam Wang <ql.wang.gd@gmail.com>
2018-11-14 23:19:25 +0800

    1. using promise instead of callbacks. 2. config whether to using md5.

--- MusicTransfer.js
+++ MusicTransfer.js
@@ -1,113 +1,134 @@
-const fs = require('fs')
-const md5File = require('md5-file')
-const path = require('path')
-const events = require('events')
+const fs = require('fs');
+const md5File = require('md5-file');
+const path = require('path');
 
-var LocalPath = '/Users/king/Music/网易云音乐'
-var PlayerPath = '/Volumes/Untitled/MUSIC'
-var localFiles = {}
-var playerFiles = {}
+const useMd5 = false;
 
-function CheckFinish(emitter, len, count) {
-	if (len === count) {
-		console.log('emit finish')
-		emitter.emit('finish')
-	}
-}
-function ReadFiles(pathName, dict, sync, cb) {
-	if (typeof cb !== 'function') {
-		throw new TypeError('Argument cb must be a function')
-	}
-	console.log(pathName)
-	fs.readdir(pathName, {withFileTypes:true}, (err, dirent) => {
-		var length = Object.keys(dirent).length
-		if (length === 0) {
-			cb()
-			return
-		} 
-		var emitter = new events.EventEmitter()
-		var count = 0
-		emitter.once('finish', () => {
-			cb()
-		})
-		dirent.forEach((d) => {
-            let f = path.join(pathName, d.name);
-            if (d.isFile() && !d.name.startsWith('.')) {
-                if (!sync) {
-					md5File(f, (err, hash, fileName) => {
-						dict[hash] = path.basename(fileName)
-						++count
-                        console.log("(%d/%d) %s, %s", count, length, path.basename(fileName), hash)
-                        CheckFinish(emitter, length, count)
-					})
-				} else {
-					var hash = md5File.sync(f)
-					dict[hash] = path.basename(f)
-                    ++count
-                    console.log("(%d/%d) %s, %s", count, length, path.basename(f), hash)
-                    CheckFinish(emitter, length, count)
-				}
-			} else {
-            	--length
-				console.log('(%d/%d) skip file %s', count, length, f)
-				CheckFinish(emitter, length, count)
-			}
-		})
-	})
-}
+var LocalPath = '/Users/king/Music/网易云音乐';
+var PlayerPath = '/Volumes/Untitled/MUSIC';
+var localFiles = {};
+var playerFiles = {};
 
-function CheckPlayerFiles(cb) {
-	if (typeof cb !== 'function') {
-		throw new TypeError('Argument cb must be a function')
-	}
-    fs.readdir(PlayerPath, {withFileTypes:true}, (err, dirent) => {
-        var length = Object.keys(dirent).length
-        var count = 0
-        var emitter = new events.EventEmitter()
-        emitter.on('delete', (fileName) => {
-            console.log('del %s', fileName )
-            fs.unlink(path.join(PlayerPath, fileName), () => {
-                CheckFinish(emitter, length, ++count)
-            })
-        })
-        emitter.once('finish', () => {
-            cb()
-        })
-		dirent.forEach((d) => {
-            if ( Object.values(localFiles).indexOf(d.name) === -1 || d.name.startsWith('.')) {
-                emitter.emit('delete', d.name)
+function readFiles(pathName, dict, sync) {
+  console.log(pathName);
+  return new Promise((resolve) => {
+    fs.readdir(pathName, {withFileTypes:true}, (err, dirent) => {
+      var length = Object.keys(dirent).length;
+      if (length === 0) {
+        resolve();
+        return;
+      } 
+      var p = [];
+      var count = 0;
+      dirent.forEach((d) => {
+        let f = path.join(pathName, d.name);
+        if (d.isFile() && !d.name.startsWith('.')) {
+          p.push(new Promise((resolve) => {
+            if (useMd5) {
+              if (!sync) {
+                md5File(f, (err, hash, fileName) => {
+                  dict[hash] = path.basename(fileName);
+                  ++count;
+                  console.log('(%d/%d) %s, %s', count, length, path.basename(fileName), hash);
+                });
+              } else {
+                var hash = md5File.sync(f);
+                dict[hash] = path.basename(f);
+                ++count;
+                console.log('(%d/%d) %s, %s', count, length, path.basename(f), hash);
+              }
             } else {
-                CheckFinish(emitter, length, ++count)
+              dict[path.basename(f)] = path.basename(f);
+              ++count;
+              console.log('(%d/%d) %s', count, length, path.basename(f));
             }
-		})
-    })
+            resolve();
+          }));  
+        } else {
+          --length;
+          console.log('(%d/%d) skip file %s', count, length, f);
+        }
+      });
+      return Promise.all(p)
+        .then(function() {
+          resolve();
+          return;
+        });
+    });
+  });
+}
+
+function delPlayerFiles() {
+  return new Promise((resolve) => {
+    fs.readdir(PlayerPath, {withFileTypes:true}, (err, dirent) => {
+      if (dirent === undefined) {
+        resolve();
+        return;
+      }
+      let p = [];
+      dirent.forEach((d) => {
+        if (Object.values(localFiles).indexOf(d.name) === -1 || d.name.startsWith('.')) {
+          p.push(new Promise((resolve) => {
+            fs.unlink(path.join(PlayerPath, d.name), () => {
+              console.log('del %s', d.name );
+              resolve();
+              return;
+            });
+          }));
+        }
+      });
+      return Promise.all(p)
+        .then(function () {
+          resolve();
+        });
+    });  
+  });
 }
 
 function CopyFiles() {
-	var emitter = new events.EventEmitter()
-	var count = 0
-	var length = Object.keys(localFiles).length - Object.keys(playerFiles).length
-    console.log("(%d/%d)", count, length)
-    emitter.on('copy', (src, dst) => {
-		fs.copyFile(src, dst, (err) => {
-			if (err) {
-				throw err;
-			}
-            ++count
-            console.log("(%d/%d) coping %s", count, length, path.basename(dst))
-        })
-	})
-	for (const [hash, fileName] of Object.entries(localFiles)) {
-		if (playerFiles[hash] === undefined) {
-			emitter.emit('copy', path.join(LocalPath, fileName), path.join(PlayerPath, fileName))
-		}
-	}
+  return new Promise((resolve) => {
+    var count = 0;
+    var length = Object.keys(localFiles).length - Object.keys(playerFiles).length;
+    console.log('(%d/%d)', count, length);
+    let p = [];
+    for (const [hash, fileName] of Object.entries(localFiles)) {
+      let key = hash;
+      if (!useMd5) {
+        key = path.basename(fileName);
+      }
+      if (playerFiles[key] === undefined) {
+        p.push(new Promise((resolve, reject) => {
+          fs.copyFile(path.join(LocalPath, fileName), path.join(PlayerPath, fileName), (err) => {
+            if (err) {
+              reject(err);
+            }
+            ++count;
+            console.log('(%d/%d) coping %s', count, length, path.basename(path.join(PlayerPath, fileName)));
+            resolve();
+          });
+        }));
+      }
+    }
+    return Promise.all(p)
+      .then(function() {
+        resolve();
+      });
+  });
 }
 
-ReadFiles(LocalPath, localFiles, false, () => {
-    CheckPlayerFiles(() => {
-		ReadFiles(PlayerPath, playerFiles, true, () => {
-			CopyFiles()
-		})
-	})
-})
+readFiles(LocalPath, localFiles, false)
+  .then(function () {
+    return delPlayerFiles();
+  })
+  .then(function () {
+    return readFiles(PlayerPath, playerFiles, true);
+  })
+  .then(function () {
+    return CopyFiles();
+  })
+  .then(function() {
+    console.log('end');
+  })
+  .catch(function(reason) {
+    console.log(reason);
+  });
